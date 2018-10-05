75f7a974338b6924de3207b52b620db1483d7786
soplinger <solekibbles4@gmail.com>
2018-10-04 22:21:50 -0400

    minecraft steve

---
+++ src/com/company/Circle.java
@@ -0,0 +1,1 @@
+package com.company; import java.util.*; public class Circle { public double radius; public Circle (double ra) { radius = ra; } public double area() { double a = Math.PI * radius * radius; return a; } public double circumference () { double c = 2 * Math.PI * radius; return c; } public double diameter () { double d = 2 * radius; return d; } }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,1 @@
-package com.company;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
+package com.company; public class Main { public static void main(String[] args) { Circle cir1 = new Circle(35.5); System.out.println(cir1.diameter()); } }
